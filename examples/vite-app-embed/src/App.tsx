018f0259e7ae4a4c459e3a892c2d4d5d76f4bf16
Zach Krall <zach@zachkrall.com>
2024-01-23 15:57:04 -0500

    add react component

--- examples/vite-app-embed/src/App.tsx
+++ examples/vite-app-embed/src/App.tsx
@@ -1,25 +1,12 @@
-import { useEffect, useRef } from 'react';
+import { EmbeddedAssistant } from '@humeai/assistant-react';
+
 import './App.css';
-import { EmbeddedAssistant } from '@humeai/assistant';
 
 function App() {
-  const embeddedAssistant = useRef<EmbeddedAssistant | null>(null);
-
-  useEffect(() => {
-    embeddedAssistant.current = EmbeddedAssistant.create({
-      rendererUrl: `http://localhost:3000`,
-    });
-    const unmount = embeddedAssistant.current.mount();
-
-    return () => {
-      unmount();
-      embeddedAssistant.current = null;
-    };
-  }, []);
-
   return (
     <>
       <div>Demo of embedding assistant as an iframe</div>
+      <EmbeddedAssistant />
     </>
   );
 }
