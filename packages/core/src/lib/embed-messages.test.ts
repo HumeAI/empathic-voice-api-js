5c69c9ab6877ce1cc69941d5db382905bd94b9f9
Zach Krall <zach@zachkrall.com>
2024-01-24 14:58:54 -0500

    add test

---
+++ packages/core/src/lib/embed-messages.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it } from 'vitest';
+
+import {
+  parseClientToFrameAction,
+  UPDATE_CONFIG_ACTION,
+} from './embed-messages';
+
+describe('parseEmbedMessage', () => {
+  it('should parse embed message', async () => {
+    const test = () => {
+      const action = UPDATE_CONFIG_ACTION({
+        apiKey: '',
+        hostname: '',
+        reconnectAttempts: 0,
+        debug: false,
+      });
+      return parseClientToFrameAction(action);
+    };
+    expect(test).not.toThrowError();
+    expect(await test()).toMatchObject({
+      type: 'update_config',
+    });
+  });
+
+  it('should reject on invalid embed message', async () => {
+    const test = () => {
+      return parseClientToFrameAction({
+        type: 'invalid',
+      });
+    };
+    await expect(test).rejects.toBeInstanceOf(Error);
+  });
+});
