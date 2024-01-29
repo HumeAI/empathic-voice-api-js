ce7de3f350c5ed60fa8e52c5874b926fcc07996f
Brian Kitano <brian@hume.ai>
2024-01-29 10:38:26 -0500

    comment

--- packages/core/src/lib/client.ts
+++ packages/core/src/lib/client.ts
@@ -21,7 +21,6 @@ export class AssistantClient {
 
   private constructor(config: Config) {
     this.url = createSocketUrl(config);
-    // this.url = `ws://localhost:8080?sample_rate=${config.sampleRate}&channels=${config.channels}`;
     this.socket = new ReconnectingWebsocket(this.url, [], {
       startClosed: true,
       maxRetries: config.reconnectAttempts,
