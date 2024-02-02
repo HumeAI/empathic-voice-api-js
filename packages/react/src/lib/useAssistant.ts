f21b714eb60b47b17d64e76281058a00b798e2eb
Yini Shi <yini@hume.ai>
2024-02-02 11:57:08 -0500

    another error handling change

--- packages/react/src/lib/useAssistant.ts
+++ packages/react/src/lib/useAssistant.ts
@@ -88,20 +88,28 @@ export const useAssistant = (props: Parameters<typeof createConfig>[0]) => {
     }
   };
 
-  const disconnect = useCallback(() => {
-    if (micPermission === 'denied') {
-      setStatus({ value: 'error', reason: 'Microphone permission denied' });
-    }
-
+  const disconnectFromAssistant = useCallback(() => {
     client.disconnect();
     player.stopAll();
     mic.stop();
-    if (status.value !== 'error') {
-      // if status was 'error', keep the error status so we can show the error message to the end user.
-      // otherwise, set status to 'disconnected'
-      setStatus({ value: 'disconnected' });
-    }
-  }, [client, player, mic, micPermission, status.value]);
+  }, [client, player, mic, micPermission]);
+
+  const disconnect = useCallback(
+    (disconnectOnError?: boolean) => {
+      if (micPermission === 'denied') {
+        setStatus({ value: 'error', reason: 'Microphone permission denied' });
+      }
+
+      disconnectFromAssistant();
+
+      if (status.value !== 'error' && !disconnectOnError) {
+        // if status was 'error', keep the error status so we can show the error message to the end user.
+        // otherwise, set status to 'disconnected'
+        setStatus({ value: 'disconnected' });
+      }
+    },
+    [client, player, mic, micPermission, status.value],
+  );
 
   useEffect(() => {
     if (
@@ -111,7 +119,7 @@ export const useAssistant = (props: Parameters<typeof createConfig>[0]) => {
     ) {
       // If the status is ever set to `error`, disconnect the assistant.
       setStatus({ value: 'error', reason: errorMessage });
-      disconnect();
+      disconnectFromAssistant();
     }
   }, [errorMessage, status.value, disconnect]);
 
