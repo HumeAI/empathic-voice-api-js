67b096083190b8712db3618f54fad4be033ab505
Zach Krall <zach@zachkrall.com>
2024-02-18 13:22:34 -0500

    add id to audio output message schema

--- packages/core/src/models/audio-output-message.ts
+++ packages/core/src/models/audio-output-message.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 export const AudioOutputMessageSchema = z
   .object({
     type: z.literal('audio_output'),
+    id: z.string(),
     data: z.string(),
   })
   .transform((obj) => {
