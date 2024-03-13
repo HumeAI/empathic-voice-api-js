1552333120523c79b0bbc96d0121f5f48c97a3c9
Mary Jiang <41601131+merryvj@users.noreply.github.com>
2024-03-13 18:04:44 -0400

    animations on emotion labels

--- apps/voice-embed-renderer/src/components/LastVoiceMessage/index.tsx
+++ apps/voice-embed-renderer/src/components/LastVoiceMessage/index.tsx
@@ -2,12 +2,59 @@ import { isExpressionColor } from '@/utils/isExpressionColor';
 import { useVoice } from '@humeai/voice-react';
 import { expressionColors } from 'expression-colors';
 import { Circle } from 'lucide-react';
-import { FC } from 'react';
+import { FC, useRef } from 'react';
+import { AnimatePresence, LayoutGroup, motion } from 'framer-motion';
 
 type ProsodyScore = { name: string; score: string };
 
 type LastVoiceMessageProps = {
   lastVoiceMessage: ReturnType<typeof useVoice>['lastVoiceMessage'];
+};
+
+const EmotionLabel = (prosody: ProsodyScore) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const fill = isExpressionColor(prosody.name)
+    ? expressionColors[prosody.name].hex
+    : 'white';
+
+  return (
+    <LayoutGroup>
+      <AnimatePresence>
+        <motion.div
+          className="mb-2 mr-2 inline-block last:mr-0"
+          style={{ minWidth: ref.current?.offsetWidth }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5, ease: 'easeIn' }}
+        >
+          <motion.div
+            ref={ref}
+            layout
+            className="rounded-full bg-tan-200/50 px-2 py-0.5 font-mono text-xs uppercase"
+          >
+            <AnimatePresence mode="wait" initial={false}>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5, ease: 'easeIn' }}
+                key={`emotion-label-${prosody.name}`}
+                className="flex items-center gap-2"
+              >
+                <Circle fill={fill} stroke={'white'} className={'size-3'} />
+                <span className="">{prosody.name}</span>
+                <span className="ml-auto tabular-nums opacity-50">
+                  {prosody.score}
+                </span>
+              </motion.div>
+            </AnimatePresence>
+          </motion.div>
+        </motion.div>
+      </AnimatePresence>
+    </LayoutGroup>
+  );
 };
 
 export const LastVoiceMessage: FC<LastVoiceMessageProps> = ({
@@ -22,22 +69,17 @@ export const LastVoiceMessage: FC<LastVoiceMessageProps> = ({
     });
 
   return (
-    <div className="pointer-events-none absolute top-48 px-4 text-center">
-      {sortedEmotions.map((emotion) => {
-        const fill = isExpressionColor(emotion.name)
-          ? expressionColors[emotion.name].hex
-          : 'white';
-
-        return (
-          <div className="mb-2 mr-2 inline-flex items-center gap-2 rounded-full bg-tan-200/50 px-2 py-0.5 font-mono text-xs uppercase last:mr-0">
-            <Circle fill={fill} stroke={'white'} className={'size-3'}/>
-            <span className="">{emotion.name}</span>
-            <span className="ml-auto tabular-nums opacity-50">
-              {emotion.score}
-            </span>
-          </div>
-        );
-      })}
+    <div className="pointer-events-none absolute top-48 px-6 text-center">
+      <LayoutGroup>
+        {sortedEmotions.map((emotion) => {
+          return (
+            <EmotionLabel
+              key={`emotion-wrapper-${emotion.name}`}
+              {...emotion}
+            />
+          );
+        })}
+      </LayoutGroup>
     </div>
   );
 };
