59adf3c8f898a1adfed8454821eb18183644c9b2
Yini Shi <yini@hume.ai>
2024-03-15 15:24:45 -0400

    loading state for connecting button (#115)

--- apps/voice-embed-renderer/src/views/ErrorScreen.tsx
+++ apps/voice-embed-renderer/src/views/ErrorScreen.tsx
@@ -1,27 +1,29 @@
+import { Button } from '@/components/Button';
 import { FC } from 'react';
 
 type ErrorScreenProps = {
   errorReason: string;
   onConnect: () => void;
+  isConnecting: boolean;
 };
 
 export const ErrorScreen: FC<ErrorScreenProps> = ({
   errorReason,
   onConnect,
+  isConnecting,
 }) => {
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="text-center">Sorry, we had to end your session.</div>
       <div>Error: {errorReason}</div>
       <div className="pt-4">
-        <button
-          className={
-            'flex h-[36px] items-center justify-center rounded-full border border-gray-700 bg-gray-800 px-4 text-base font-medium text-white hover:bg-gray-700 hover:text-white focus:bg-gray-700 focus:text-white focus:outline-none'
-          }
+        <Button
           onClick={onConnect}
+          isLoading={isConnecting}
+          loadingText={'Connecting...'}
         >
           Reconnect
-        </button>
+        </Button>
       </div>
     </div>
   );
