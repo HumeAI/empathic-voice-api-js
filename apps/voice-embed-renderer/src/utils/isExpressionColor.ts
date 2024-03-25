2571864c6a4b35cd4ea3b60c88e24db4f1ce68af
Yini Shi <yini@hume.ai>
2024-03-25 14:58:52 -0400

    remove voice embed renderer from sdk package

--- apps/voice-embed-renderer/src/utils/isExpressionColor.ts
+++
@@ -1,7 +0,0 @@
-import { expressionColors } from "expression-colors";
-
-export function isExpressionColor(
-  s: string | number | symbol,
-): s is keyof typeof expressionColors {
-  return s in expressionColors;
-}
--- apps/voice-embed-renderer/src/utils/parentDispatch.ts
+++
@@ -1,6 +0,0 @@
-import { type FrameToClientAction } from '@humeai/voice-embed-react';
-
-export const parentDispatch = (action: FrameToClientAction) => {
-  console.log('[parent dispatch] sending message', action);
-  window.parent.postMessage(action, '*');
-};
