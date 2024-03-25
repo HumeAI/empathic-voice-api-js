58547be045f2abe279ee9f9539779322b93d6cbc
Yini Shi <yini@hume.ai>
2024-03-25 16:17:38 -0400

    remove voice embed renderer from sdk package (#140)

--- apps/voice-embed-renderer/src/components/WebGLAvatar/shaders/screenQuadVertexShader.glsl
+++
@@ -1,8 +0,0 @@
-precision mediump float;
-
-varying vec2 vUv;
-
-void main() {
-  vUv = (uv);
-  gl_Position = vec4(position.xy, 0.0, 1.0);
-}
--- apps/voice-embed-renderer/src/utils/useSafeMotion.ts
+++
@@ -1,8 +0,0 @@
-import { MotionProps, useReducedMotion } from 'framer-motion';
-
-// @reference https://github.com/framer/motion/issues/1723
-export const useSafeMotion = (motion: MotionProps): MotionProps => {
-  const reduced = useReducedMotion();
-
-  return reduced ? {} : motion;
-};
