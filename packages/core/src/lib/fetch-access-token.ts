9cab72e0383eed8161e091ef3d44498bf7a843b4
Zachary Greathouse <26175515+zgreathouse@users.noreply.github.com>
2024-04-09 15:48:38 -0700

    Zg/fetch access token (#165)

--- packages/core/src/lib/fetch-access-token.ts
+++ packages/core/src/lib/fetch-access-token.ts
@@ -1,18 +1,40 @@
 /**
- * @name fetchAccessToken
- * @description
- * Fetch an access token from the Hume API.
+ * Function which detects whether the function is being ran in the browser or node environment,
+ * and base64 encodes the string input using the natively available method.
+ *
+ * @param str
+ * @returns base64 encoded string
+ */
+function base64Encode(str: string): string {
+  if (typeof Buffer === 'function') {
+    // Node.js environment
+    return Buffer.from(str).toString('base64');
+  } else if (typeof btoa === 'function') {
+    // Browser environment
+    return btoa(str);
+  } else {
+    throw new Error(
+      'Base64 encoding is not natively supported in this environment.',
+    );
+  }
+}
+
+/**
+ * Fetches a new access token from the Hume API using the provided API key and client secret.
+ *
  * @param args - The arguments for the request.
- * @returns
- * A new access token.
+ * @returns Promise that resolves to the new access token.
+ * @throws If the base64 encoding fails.
+ * @throws If the network request fails.
  * @example
- * ```ts
- * const accessToken = await fetchAccessToken({
- * apiKey: '
- * test',
- * clientSecret
- * : 'test',
- * });
+ * ```typescript
+ * async function getToken() {
+ *   const accessToken = await fetchAccessToken({
+ *     apiKey: 'test',
+ *     clientSecret: 'test',
+ *   });
+ *   console.log(accessToken); // Outputs the access token
+ * }
  * ```
  */
 export const fetchAccessToken = async (args: {
@@ -23,7 +45,7 @@ export const fetchAccessToken = async (args: {
   const { apiKey, clientSecret, host = 'api.hume.ai' } = args;
 
   const authString = `${apiKey}:${clientSecret}`;
-  const encoded = Buffer.from(authString).toString('base64');
+  const encoded = base64Encode(authString);
 
   const res = await fetch(`https://${host}/oauth2-cc/token`, {
     method: 'POST',
@@ -38,7 +60,7 @@ export const fetchAccessToken = async (args: {
   });
 
   const data = (await res.json()) as { access_token: string };
-
   const accessToken = String(data['access_token']);
+
   return accessToken;
 };
