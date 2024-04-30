d275c2f3216769b805b6165e250cdea48fbc0062
Zach Krall <zach@zachkrall.com>
2024-04-30 13:06:25 -0400

    add received at

--- packages/core/src/models/tool-messages.ts
+++ packages/core/src/models/tool-messages.ts
@@ -7,36 +7,57 @@ const jsonSchema: z.ZodType<Json> = z.lazy(() =>
   z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)]),
 );
 
-export const ToolCallSchema = z.object({
-  type: z.literal('tool_call'),
-  tool_type: z.enum(['builtin', 'function']),
-  tool_call_id: z.string(),
-  response_required: z.boolean(),
-  name: z.string(),
-  parameters: z.string(),
-});
+// MARK: ToolCall Message
+export const ToolCallSchema = z
+  .object({
+    type: z.literal('tool_call'),
+    tool_type: z.enum(['builtin', 'function']),
+    tool_call_id: z.string(),
+    response_required: z.boolean(),
+    name: z.string(),
+    parameters: z.string(),
+  })
+  .transform((obj) => {
+    return Object.assign(obj, {
+      receivedAt: new Date(),
+    });
+  });
 
 export type ToolCall = z.infer<typeof ToolCallSchema>;
 
+// MARK: ToolResponse Message
 export const ToolResponseContentSchema = z.union([z.string(), jsonSchema]);
 
 export type ToolResponseContent = z.infer<typeof ToolResponseContentSchema>;
 
-export const ToolResponseSchema = z.object({
-  type: z.literal('tool_response'),
-  tool_call_id: z.string(),
-  content: ToolResponseContentSchema,
-});
+export const ToolResponseSchema = z
+  .object({
+    type: z.literal('tool_response'),
+    tool_call_id: z.string(),
+    content: ToolResponseContentSchema,
+  })
+  .transform((obj) => {
+    return Object.assign(obj, {
+      receivedAt: new Date(),
+    });
+  });
 
 export type ToolResponse = z.infer<typeof ToolResponseSchema>;
 
-export const ToolErrorSchema = z.object({
-  type: z.literal('tool_error'),
-  tool_call_id: z.string(),
-  content: z.string().nullish(),
-  error: z.string(),
-  code: z.string(),
-  level: z.string(),
-});
+// MARK: ToolError Message
+export const ToolErrorSchema = z
+  .object({
+    type: z.literal('tool_error'),
+    tool_call_id: z.string(),
+    content: z.string().nullish(),
+    error: z.string(),
+    code: z.string(),
+    level: z.string(),
+  })
+  .transform((obj) => {
+    return Object.assign(obj, {
+      receivedAt: new Date(),
+    });
+  });
 
 export type ToolError = z.infer<typeof ToolErrorSchema>;
