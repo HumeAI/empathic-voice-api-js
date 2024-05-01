a1cc442902b4bd2029040241e577b7359f9c7a0a
Zach Krall <zach@zachkrall.com>
2024-05-01 10:48:13 -0400

    start test file for useVoice

---
+++ packages/react/src/lib/VoiceProvider.test.tsx
@@ -0,0 +1,22 @@
+import { renderHook } from '@testing-library/react';
+import React from 'react';
+import { describe, expect, it } from 'vitest';
+
+import { VoiceReadyState } from './useVoiceClient';
+import { useVoice, VoiceProvider } from './VoiceProvider';
+
+describe('useVoice', () => {
+  it('should in', async () => {
+    const hook = renderHook(() => useVoice(), {
+      wrapper: ({ children }) => {
+        return (
+          <VoiceProvider auth={{ type: 'apiKey', value: 'abc' }}>
+            {children}
+          </VoiceProvider>
+        );
+      },
+    });
+
+    expect(hook.result.current.readyState).toBe(VoiceReadyState.IDLE);
+  });
+});
