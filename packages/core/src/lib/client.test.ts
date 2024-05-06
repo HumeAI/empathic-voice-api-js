c11593a3fd6757581819f5060d5d86a1ef5c2907
Yini Shi <yini@hume.ai>
2024-05-06 17:00:20 -0400

    don't throw error if message type is unrecognized'

--- packages/core/src/lib/client.test.ts
+++ packages/core/src/lib/client.test.ts
@@ -1,8 +1,16 @@
 import { match } from 'ts-pattern';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 import { VoiceClient } from './client';
 import { defaultConfig } from './create-socket-config';
+
+import { flushPromises } from '@/test-utils/flushPromises';
+
+vi.mock('./message', () => {
+  return {
+    parseMessageType: vi.fn().mockResolvedValue({ success: false }),
+  };
+});
 
 describe('client', () => {
   it('to start closed', () => {
@@ -50,5 +58,26 @@ describe('client', () => {
     });
 
     expect(client.readyState).toBe(3);
+  });
+
+  it('should not call the message event handler if parseMessageType returns a failure', async () => {
+    const client = VoiceClient.create({
+      ...defaultConfig,
+      auth: {
+        type: 'apiKey',
+        value: 'test',
+      },
+    });
+
+    const messageHandler = vi.fn();
+    client.on('message', messageHandler);
+
+    const testEvent = new MessageEvent('message', {
+      data: JSON.stringify({ type: 'test_message', payload: 'test' }),
+    });
+    client.handleMessage(testEvent);
+
+    await flushPromises();
+    expect(messageHandler).not.toHaveBeenCalled();
   });
 });
