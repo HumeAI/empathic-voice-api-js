00eff3117fdacd769d78912398bc88e99b374b9a
Franc Camps Febrer <francamps@gmail.com>
2024-07-24 16:11:27 -0400

    Extend types with receivedAt

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -29,16 +29,30 @@ export type {
 
 export type { SocketConfig } from './lib/useVoiceClient';
 
-export type {
-  AssistantEnd,
-  AssistantMessage,
-  AudioInput,
-  AudioOutput,
-  ChatMetadata,
-  SubscribeEvent,
-  ToolCallMessage,
-  ToolErrorMessage,
-  ToolResponseMessage,
-  UserInterruption,
-  UserMessage,
+import {
+  AssistantEnd as OriginalAssistantEnd,
+  AssistantMessage as OriginalAssistantMessage,
+  AudioInput as OriginalAudioInput,
+  AudioOutput as OriginalAudioOutput,
+  ChatMetadata as OriginalChatMetadata,
+  SubscribeEvent as OriginalSubscribeEvent,
+  ToolCallMessage as OriginalToolCallMessage,
+  ToolErrorMessage as OriginalToolErrorMessage,
+  ToolResponseMessage as OriginalToolResponseMessage,
+  UserInterruption as OriginalUserInterruption,
+  UserMessage as OriginalUserMessage,
 } from 'hume/api/resources/empathicVoice';
+
+type WithReceivedAt<T> = T & { receivedAt: Date };
+
+export type AssistantEnd = WithReceivedAt<OriginalAssistantEnd>;
+export type AssistantMessage = WithReceivedAt<OriginalAssistantMessage>;
+export type AudioInput = WithReceivedAt<OriginalAudioInput>;
+export type AudioOutput = WithReceivedAt<OriginalAudioOutput>;
+export type ChatMetadata = WithReceivedAt<OriginalChatMetadata>;
+export type SubscribeEvent = WithReceivedAt<OriginalSubscribeEvent>;
+export type ToolCallMessage = WithReceivedAt<OriginalToolCallMessage>;
+export type ToolErrorMessage = WithReceivedAt<OriginalToolErrorMessage>;
+export type ToolResponseMessage = WithReceivedAt<OriginalToolResponseMessage>;
+export type UserInterruption = WithReceivedAt<OriginalUserInterruption>;
+export type UserMessage = WithReceivedAt<OriginalUserMessage>;
