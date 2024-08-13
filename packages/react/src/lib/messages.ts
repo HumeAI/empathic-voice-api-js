c40c8273330d5fe4bf4299231e3cc0b7574f4d34
dsinghvi <deep@usebirch.com>
2024-08-13 14:31:01 -0400

    (fix): introduce errors and parse message data func

---
+++ packages/react/src/lib/messages.ts
@@ -0,0 +1,121 @@
+import { type Hume } from 'hume';
+import * as serializers from 'hume/serialization';
+
+import {
+  SocketFailedToParseMessageError,
+  SocketUnknownMessageError,
+} from './errors';
+
+export const parseAudioMessage = async (
+  blob: Blob,
+): Promise<Hume.empathicVoice.AudioOutput | null> => {
+  return blob
+    .arrayBuffer()
+    .then((buffer) => {
+      return {
+        type: 'audio_output',
+        id: '',
+        data: buffer,
+        receivedAt: new Date(),
+      };
+    })
+    .catch(() => {
+      return null;
+    });
+};
+
+/**
+ * @name parseMessageData
+ * @description
+ * Parse the data of a message from the socket.
+ * @param data - The data to parse.
+ * @returns
+ * The parsed message data.
+ * @example
+ * ```ts
+ * const message = await parseMessageData(data);
+ * ```
+ */
+export const parseMessageData = async (
+  data: unknown,
+): Promise<
+  | {
+      success: true;
+      message: Hume.empathicVoice.SubscribeEvent;
+    }
+  | {
+      success: false;
+      error: Error;
+    }
+> => {
+  if (data instanceof Blob) {
+    const message = await parseAudioMessage(data);
+
+    if (message) {
+      return {
+        success: true,
+        message,
+      };
+    } else {
+      return {
+        success: false,
+        error: new SocketFailedToParseMessageError(
+          `Received blob was unable to be converted to ArrayBuffer.`,
+        ),
+      };
+    }
+  }
+
+  if (typeof data !== 'string') {
+    return {
+      success: false,
+      error: new SocketFailedToParseMessageError(
+        `Expected a string but received ${typeof data}.`,
+      ),
+    };
+  }
+
+  const parseResponse = serializers.empathicVoice.SubscribeEvent.parse(data);
+
+  if (!parseResponse.ok) {
+    return {
+      success: false,
+      error: new SocketUnknownMessageError(
+        `Received JSON was not a known message type.`,
+      ),
+    };
+  }
+
+  return {
+    success: true,
+    message: parseResponse.value,
+  };
+};
+
+/**
+ * @name parseMessageType
+ * @description
+ * Parse the type of a message from the socket.
+ * @param event - The event to parse.
+ * @returns
+ * The parsed message type.
+ * @example
+ * ```ts
+ * const message = await parseMessageType(event);
+ * ```
+ */
+export const parseMessageType = async (
+  event: MessageEvent,
+): Promise<
+  | {
+      success: true;
+      message: Hume.empathicVoice.SubscribeEvent;
+    }
+  | {
+      success: false;
+      error: Error;
+    }
+> => {
+  const data: unknown = event.data;
+  return parseMessageData(data);
+};
